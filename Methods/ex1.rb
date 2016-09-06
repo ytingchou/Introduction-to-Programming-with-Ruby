2dce2f04e27c375424b96470522d040e25d14a96
Yuting Chou <ytingchou@gmail.com>
2016-09-06 15:00:29 +0800

    Added exercise file to "Methods" chapter.

---
+++ Methods/ex1.rb
@@ -0,0 +1,9 @@
+# Write a program that prints a greeting message. This program should
+# contain a method called greeting that takes a name as its parameter and
+# returns a string.
+def greeting(name)
+  "Hello #{name}!"
+end
+
+puts greeting("Yuting")
+
